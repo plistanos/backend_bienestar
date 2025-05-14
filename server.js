51f9204528b2f1c73af4ca9ebbdc91e92abb0ce4
plistanos <michaelvilches747@gmail.com>
2025-05-14 17:00:12 -0400

    add front

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Conectar a MongoDB
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI+"/plataforma-bienestar")
 .then(() => console.log("Conectado a MongoDB"))
 .catch(err => console.error("Error de conexión a MongoDB:", err));
 
